ca217175827efa682453b85c13e63799bb3943a5
Veronika Shakun <veronika.shakun@gmail.com>
2021-02-08 22:24:26 +0300

    Post implementation

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,17 +1,44 @@
 import React from "react";
+import {addUserIfNotExists, selectCurrentUserUUID} from "../auth/authSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {likePost} from '../feed/feedSlice';
+import {Link} from "react-router-dom";
 
 export interface IPost {
     author: string,
     text: string,
-    liked: Array<string>
+    liked: Array<string>,
+    timeStamp: number
+}
+
+export interface IUserPost {
+    userUUID: string,
+    postUUID: string
 }
 
 export function Post(props: any) {
+    const dispatch = useDispatch();
+    const currentUserUUID = useSelector(selectCurrentUserUUID);
+
     return (
         <div>
-            <p>{props.post.author}</p>
+            <Link
+                to={`/profile/${props.post.author}`}>
+                <p>{props.post.author}</p>
+            </Link>
             <p>{props.post.text}</p>
             <p>Liked: {props.post.liked.join(', ')}</p>
+            <button
+                onClick={() => {
+                    const userPost: IUserPost = {
+                        userUUID: currentUserUUID || '',
+                        postUUID: props.uuid
+                    };
+                    dispatch(likePost(userPost));
+                }}
+            >
+                ❤
+            </button>
         </div>
     )
 }
